67c74532fa67d7107463802da926b002e67e3fea
Emanuel López Reyes <elyon.king.dcn@gmail.com>
2025-01-01 14:28:43 -0500

    Update menu.js

--- arch/menu.js
+++ arch/menu.js
@@ -10,10 +10,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     menuContainer.innerHTML = `
     <div style="display: flex; justify-content: space-around;">
-    <div class="menu-item" onclick="window.location.href='openeduk.github.io/index.html'">Inicio</div>
-    <div class="menu-item" onclick="window.location.href='openeduk.github.io/cursos.html'">Cursos</div>
-    <div class="menu-item" onclick="window.location.href='openeduk.github.io/sobre_nosotros.html'">Acerca de</div>
-    <div class="menu-item" onclick="window.location.href='openeduk.github.io/contactanos.html'">Contáctanos</div>
+    <div class="menu-item" onclick="window.location.href='https://openeduk.github.io/index.html'">Inicio</div>
+    <div class="menu-item" onclick="window.location.href='https://openeduk.github.io/cursos.html'">Cursos</div>
+    <div class="menu-item" onclick="window.location.href='https://openeduk.github.io/sobre_nosotros.html'">Acerca de</div>
+    <div class="menu-item" onclick="window.location.href='https://openeduk.github.io/contactanos.html'">Contáctanos</div>
     </div>
     `;
 
