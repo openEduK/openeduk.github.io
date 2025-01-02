9c798a760a8fb5c5832978b55900a6fea9fe1305
Emanuel López Reyes <elyon.king.dcn@gmail.com>
2025-01-02 08:11:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Animación de entrada para los elementos del main
     const sections = document.querySelectorAll("main section");
     sections.forEach((section, index) => {
         section.style.opacity = 0;
@@ -8,6 +7,15 @@ document.addEventListener("DOMContentLoaded", function() {
             section.style.transition = "opacity 0.5s ease, transform 0.5s ease";
             section.style.opacity = 1;
             section.style.transform = "translateY(0)";
-        }, index * 300); // Retraso en la animación para cada sección
+        }, index * 300);
     });
+    
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('nombre-proyecto').innerText = data.proyecto;
+            document.getElementById('extension').innerText = data.extension;
+            document.getElementById('correo').innerText = data.correo;
+        })
+        .catch(error => console.error('Error al cargar el archivo JSON:', error));
 });
